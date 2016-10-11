dcb9452f23388373d1518296c8bc9f1836f3c7cc
Will Retzloff <wretzloff@gmail.com>
2016-10-10 23:40:16 -0500

    Refactor

--- com.example.helloworld/src/com/example/helloworld/HelloWorldView.java
+++ com.example.helloworld/src/com/example/helloworld/HelloWorldView.java
@@ -66,15 +66,15 @@ package com.example.helloworld;
       public IProject copyProject(String projectName) throws CoreException 
       {
     	    IProgressMonitor m = new NullProgressMonitor();
-    	    IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
-    	    IProject project = workspaceRoot.getProject(projectName);
+    	    //IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+    	    IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
     	    IProjectDescription projectDescription = project.getDescription();
     	    String cloneName = projectName + "_copy";
     	    // create clone project in workspace
-    	    IProjectDescription cloneDescription = workspaceRoot.getWorkspace().newProjectDescription(cloneName);
+    	    IProjectDescription cloneDescription = ResourcesPlugin.getWorkspace().newProjectDescription(cloneName);
     	    // copy project files
     	    project.copy(cloneDescription, true, m);
-    	    IProject clone = workspaceRoot.getProject(cloneName);
+    	    IProject clone = ResourcesPlugin.getWorkspace().getRoot().getProject(cloneName);
     	    // copy the project properties
     	    cloneDescription.setNatureIds(projectDescription.getNatureIds());
     	    cloneDescription.setReferencedProjects(projectDescription.getReferencedProjects());
@@ -91,11 +91,8 @@ package com.example.helloworld;
     	  //Create a temporary ArrayList to hold the names of the available projects
     	  ArrayList<String> projectsList = new ArrayList<String>();
     	  
-    	  // Get the root of the workspace
-    	  IWorkspace workspace = ResourcesPlugin.getWorkspace();
-    	  IWorkspaceRoot root = workspace.getRoot();
     	  // Get all projects in the workspace
-    	  IProject[] projects = root.getProjects();
+    	  IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
     	  // Loop over all projects
     	  for (IProject project : projects) 
     	  {
@@ -119,9 +116,7 @@ package com.example.helloworld;
     			  copyProject(projectName);
 				
     			  //Get a handle to the copy
-    			  IWorkspace workspace = ResourcesPlugin.getWorkspace();
-    			  IWorkspaceRoot root = workspace.getRoot();
-    			  IProject projectCopy = root.getProject(projectName + "_copy");
+    			  IProject projectCopy = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName + "_copy");
     			  textStatusArea.append("New project name: " + projectCopy.getName() + "\n");
 		    	
     			  //Create an AST for the project
