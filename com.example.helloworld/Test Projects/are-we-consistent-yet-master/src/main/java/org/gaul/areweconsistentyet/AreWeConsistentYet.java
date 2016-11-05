8fc353d1cfeaebe6a4add8f9bd93f4ad35430f79
Will Retzloff <wretzloff@gmail.com>
2016-11-05 09:15:53 -0500

    Adding test projects to run this plugin against

---
+++ com.example.helloworld/Test Projects/are-we-consistent-yet-master/src/main/java/org/gaul/areweconsistentyet/AreWeConsistentYet.java
@@ -0,0 +1,307 @@
+/*
+ * Copyright 2014 Andrew Gaul <andrew@gaul.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gaul.areweconsistentyet;
+
+import static java.util.Objects.requireNonNull;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Random;
+import java.util.Set;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.ByteSource;
+import com.google.common.io.ByteStreams;
+import com.google.inject.Module;
+
+import org.jclouds.Constants;
+import org.jclouds.ContextBuilder;
+import org.jclouds.blobstore.BlobStore;
+import org.jclouds.blobstore.BlobStoreContext;
+import org.jclouds.blobstore.domain.Blob;
+import org.jclouds.blobstore.domain.PageSet;
+import org.jclouds.blobstore.domain.StorageMetadata;
+import org.jclouds.blobstore.options.ListContainerOptions;
+import org.jclouds.domain.Location;
+import org.jclouds.io.Payload;
+import org.jclouds.logging.slf4j.config.SLF4JLoggingModule;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+public final class AreWeConsistentYet {
+    private final ByteSource payload1;
+    private final ByteSource payload2;
+    private final BlobStore blobStore;
+    private final BlobStore blobStoreRead;
+    private final String containerName;
+    private final int iterations;
+    private final Random random = new Random();
+
+    public AreWeConsistentYet(BlobStore blobStore,
+            BlobStore blobStoreRead, String containerName, int iterations,
+            long objectSize) {
+        this.blobStore = requireNonNull(blobStore);
+        this.blobStoreRead = requireNonNull(blobStoreRead);
+        this.containerName = requireNonNull(containerName);
+        checkArgument(iterations > 0,
+                "iterations must be greater than zero, was: " + iterations);
+        this.iterations = iterations;
+        checkArgument(objectSize > 0,
+                "object size must be greater than zero, was: " + objectSize);
+        payload1 = Utils.infiniteByteSource((byte) 1).slice(0, objectSize);
+        payload2 = Utils.infiniteByteSource((byte) 2).slice(0, objectSize);
+    }
+
+    public int readAfterCreate() throws IOException {
+        int count = 0;
+        for (int i = 0; i < iterations; ++i) {
+            String blobName = makeBlobName();
+            blobStore.putBlob(containerName, makeBlob(blobName, payload1));
+            Blob getBlob = blobStoreRead.getBlob(containerName, blobName);
+            if (getBlob == null) {
+                ++count;
+            } else {
+                try (Payload payload = getBlob.getPayload();
+                     InputStream is = payload.openStream()) {
+                    ByteStreams.copy(is, ByteStreams.nullOutputStream());
+                }
+            }
+            blobStore.removeBlob(containerName, blobName);
+        }
+        return count;
+    }
+
+    public int readAfterDelete() throws IOException {
+        int count = 0;
+        for (int i = 0; i < iterations; ++i) {
+            String blobName = makeBlobName();
+            blobStoreRead.putBlob(containerName, makeBlob(blobName, payload1));
+            blobStore.removeBlob(containerName, blobName);
+            Blob getBlob = blobStoreRead.getBlob(containerName, blobName);
+            if (getBlob != null) {
+                ++count;
+                try (Payload payload = getBlob.getPayload();
+                     InputStream is = payload.openStream()) {
+                    ByteStreams.copy(is, ByteStreams.nullOutputStream());
+                }
+            }
+        }
+        return count;
+    }
+
+    public int readAfterOverwrite() throws IOException {
+        int count = 0;
+        for (int i = 0; i < iterations; ++i) {
+            String blobName = makeBlobName();
+            blobStore.putBlob(containerName, makeBlob(blobName, payload1));
+            blobStore.putBlob(containerName, makeBlob(blobName, payload2));
+            Blob getBlob = blobStoreRead.getBlob(containerName, blobName);
+            if (getBlob == null) {
+                ++count;
+                continue;
+            }
+            try (Payload payload = getBlob.getPayload();
+                 InputStream is = payload.openStream()) {
+                if (Arrays.equals(payload1.read(), ByteStreams.toByteArray(
+                        is))) {
+                    ++count;
+                }
+            }
+            blobStore.removeBlob(containerName, blobName);
+        }
+        return count;
+    }
+
+    public int listAfterCreate() throws IOException {
+        int count = 0;
+        for (int i = 0; i < iterations; ++i) {
+            String blobName = makeBlobName();
+            blobStore.putBlob(containerName, makeBlob(blobName, payload1));
+            if (!listAllBlobs().contains(blobName)) {
+                ++count;
+            }
+            blobStore.removeBlob(containerName, blobName);
+        }
+        return count;
+    }
+
+    public int listAfterDelete() throws IOException {
+        int count = 0;
+        for (int i = 0; i < iterations; ++i) {
+            String blobName = makeBlobName();
+            blobStoreRead.putBlob(containerName, makeBlob(blobName, payload1));
+            blobStore.removeBlob(containerName, blobName);
+            if (listAllBlobs().contains(blobName)) {
+                ++count;
+            }
+        }
+        return count;
+    }
+
+    private String makeBlobName() {
+        return "blob-name-" + random.nextInt();
+    }
+
+    private Blob makeBlob(String blobName, ByteSource payload)
+            throws IOException {
+        return blobStore.blobBuilder(blobName)
+                .payload(payload)
+                .contentLength(payload.size())
+                .build();
+    }
+
+    private Set<String> listAllBlobs() {
+        Set<String> blobNames = new HashSet<String>();
+        ListContainerOptions options = new ListContainerOptions();
+        while (true) {
+            PageSet<? extends StorageMetadata> set = blobStoreRead.list(
+                    containerName, options);
+            for (StorageMetadata sm : set) {
+                blobNames.add(sm.getName());
+            }
+            String marker = set.getNextMarker();
+            if (marker == null) {
+                break;
+            }
+            options = options.afterMarker(marker);
+        }
+        return blobNames;
+    }
+
+    static final class Options {
+        @Option(name = "--container-name",
+                usage = "container name for tests, will be created and removed",
+                required = true)
+        private String containerName;
+
+        @Option(name = "--iterations",
+                usage = "number of iterations (default: 1)")
+        private int iterations = 1;
+
+        @Option(name = "--location", usage = "container location")
+        private String location;
+
+        @Option(name = "--size", usage = "object size in bytes (default: 1)")
+        private long objectSize = 1;
+
+        @Option(name = "--properties", usage = "configuration file",
+                required = true)
+        private File propertiesFile;
+
+        @Option(name = "--reader-endpoint",
+                usage = "separate endpoint to read from")
+        private String readerEndpoint;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Options options = new Options();
+        CmdLineParser parser = new CmdLineParser(options);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException cle) {
+            PrintStream err = System.err;
+            err.println("are-we-consistent-yet version " +
+                    AreWeConsistentYet.class.getPackage()
+                            .getImplementationVersion());
+            err.println("Usage: are-we-consistent-yet" +
+                    " --container-name NAME --properties FILE [options...]");
+            parser.printUsage(err);
+            System.exit(1);
+        }
+
+        Properties properties = new Properties();
+        try (InputStream is = new FileInputStream(options.propertiesFile)) {
+            properties.load(is);
+        }
+        Properties propertiesRead = (Properties) properties.clone();
+        if (options.readerEndpoint != null) {
+            propertiesRead.setProperty(Constants.PROPERTY_ENDPOINT,
+                    options.readerEndpoint);
+        }
+
+        try (BlobStoreContext context = blobStoreContextFromProperties(
+                     properties);
+             BlobStoreContext contextRead = blobStoreContextFromProperties(
+                     propertiesRead)) {
+            BlobStore blobStore = context.getBlobStore();
+            BlobStore blobStoreRead = contextRead.getBlobStore();
+
+            Location location = null;
+            if (options.location != null) {
+                for (Location loc : blobStore.listAssignableLocations()) {
+                    if (loc.getId().equalsIgnoreCase(options.location)) {
+                        location = loc;
+                        break;
+                    }
+                }
+                if (location == null) {
+                    throw new Exception("Could not find location: " +
+                            options.location);
+                }
+            }
+            blobStore.createContainerInLocation(location,
+                    options.containerName);
+            AreWeConsistentYet test = new AreWeConsistentYet(
+                    blobStore, blobStoreRead, options.containerName,
+                    options.iterations, options.objectSize);
+            PrintStream out = System.out;
+            out.println("eventual consistency count with " +
+                    options.iterations + " iterations: ");
+            out.println("read after create: " + test.readAfterCreate());
+            out.println("read after delete: " + test.readAfterDelete());
+            out.println("read after overwrite: " + test.readAfterOverwrite());
+            out.println("list after create: " + test.listAfterCreate());
+            out.println("list after delete: " + test.listAfterDelete());
+            blobStore.deleteContainer(options.containerName);
+        }
+    }
+
+    private static BlobStoreContext blobStoreContextFromProperties(
+            Properties properties) {
+        String provider = properties.getProperty(Constants.PROPERTY_PROVIDER);
+        String identity = properties.getProperty(Constants.PROPERTY_IDENTITY);
+        String credential = properties.getProperty(
+                Constants.PROPERTY_CREDENTIAL);
+        String endpoint = properties.getProperty(Constants.PROPERTY_ENDPOINT);
+        if (provider == null || identity == null || credential == null) {
+            System.err.println("Properties file must contain:\n" +
+                    Constants.PROPERTY_PROVIDER + "\n" +
+                    Constants.PROPERTY_IDENTITY + "\n" +
+                    Constants.PROPERTY_CREDENTIAL);
+            System.exit(1);
+        }
+
+        ContextBuilder builder = ContextBuilder
+                .newBuilder(provider)
+                .credentials(identity, credential)
+                .modules(ImmutableList.<Module>of(new SLF4JLoggingModule()))
+                .overrides(properties);
+        if (endpoint != null) {
+            builder = builder.endpoint(endpoint);
+        }
+        return builder.build(BlobStoreContext.class);
+    }
+}
---
+++ com.example.helloworld/Test Projects/ashot-master/README.md
@@ -0,0 +1,131 @@
+aShot
+=====
+
+
+[![release](http://github-release-version.herokuapp.com/github/yandex-qatools/ashot/release.svg?style=flat)](https://github.com/yandex-qatools/ashot/releases/latest) [![Maven Central](https://maven-badges.herokuapp.com/maven-central/ru.yandex.qatools.ashot/ashot/badge.svg?style=flat)](https://maven-badges.herokuapp.com/maven-central/ru.yandex.qatools.ashot/ashot)
+
+
+WebDriver Screenshot utility
+
+* Takes a screenshot of the WebElement on different platforms (i.e. desktop browsers, iOS Simulator Mobile Safari, Android Emulator Browser)
+* Decorates screenshots
+* Provides flexible screenshot comparison
+
+#####WebElement view
+
+aShot takes a screenshot in three simple steps:
+* Capture the entire page screenshot
+* Find element's size and position   
+* Crop the original screenshot
+
+As a result aShot provides an image of the WebElement
+![images snippet](/doc/img/images_intent_blur.png)
+
+#### Maven dependency
+```xml
+<dependency>
+    <groupId>ru.yandex.qatools.ashot</groupId>
+    <artifactId>ashot</artifactId>
+    <version>1.5.2</version>
+</dependency>
+``` 
+
+#####Capturing the entire page
+
+Different WebDrivers take screenshots in different ways. Some WebDrivers provide a screenshot of the entire page while others handle the viewport only. aShot might be configured to handle browsers with the viewport problem. This example configuration gives a screenshot of the entire page even for Chrome, Mobile Safari, etc. 
+```java
+new AShot()
+  .shootingStrategy(ShootingStrategies.viewportPasting(100))
+  .takeScreenshot(webDriver);
+```
+
+There are more build in strategies in ```ShootingStrategies``` for different use cases. In case there are no suitable strategy it is possible to build it using existing strategies as decorators or implement your own.
+```
+CutStrategy cutting = new VariableCutStrategy(HEADER_IOS_8_MIN, HEADER_IOS_8_MAX, VIEWPORT_MIN_IOS_8_SIM);
+ShootingStrategy rotating = new RotatingDecorator(cutting, ShootingStrategies.simple());
+ShootingStrategy pasting = new ViewportPastingDecorator(rotating)
+    .withScrollTimeout(scrollTimeout);   
+new AShot()
+  .shootingStrategy(pasting)
+  .takeScreenshot(webDriver);
+```
+
+#####Capturing the WebElement
+
+One can take a screenshot of the particular WebElement(s). Just specify the element(s).
+```java
+WebElement myWebElement = webDriver.findElement(By.cssSelector("#my_element"));
+new AShot()
+  .takeScreenshot(webDriver, myWebElement);
+```
+ 
+As noted earlier aShot will find element's size and position and crop original image. WebDriver API provides a method to find the WebElement's coordinates but different WebDriver implementations behave differently. In my opinion the most general approach to find coordinates is to use jQuery. So aShot uses jQuery by default. But some drivers have problems with Javascript execution such as OperaDriver. In this case there is another way to find the WebElement's coordinates.
+```java
+new AShot()
+  .coordsProvider(new WebDriverCoordsProvider()) //find coordinates with WebDriver API
+  .takeScreenshot(webDriver, myWebElement);
+```
+Feel free to implement own CoordsProvider. Pull requests are welcome.
+
+#####Prettifying the screenshot
+
+So, let's take a simple screenshot of the weather snippet at Yandex.com.
+
+```java
+new AShot()
+  .takeScreenshot(webDriver, yandexWeatherElement);
+```
+Here is the result.
+![simple weather snippet](/doc/img/def_crop.png)
+ 
+```DefaultCropper``` is used by default. Can we do better? Yes, we can.
+ 
+```java
+new AShot()
+  .withCropper(new IndentCropper()           // set custom cropper with indentation
+                 .addIndentFilter(blur()))   // add filter for indented areas
+  .takeScreenshot(driver, yandexWeatherElement);
+```
+  
+![indent blur weather snippet](/doc/img/weather_indent_blur.png)
+This screenshot provides more information about position relative other elements and blurs indent in order to focus on the WebElement.  
+  
+#####Screenshot comparison
+```.takeScreenshot()``` returns a ```Screenshot``` object which contains an image and data for comparison. One can ignore some WebElements from comparison.
+
+```java
+Screenshot myScreenshot = new AShot()
+  .addIgnoredElement(By.cssSelector("#weather .blinking_element")) // ignored element(s)
+  .takeScreenshot(driver, yandexWeatherElement);
+```
+
+Use ```ImageDiffer``` to find a difference between two images.
+
+```java
+ImageDiff diff = new ImageDiffer().makeDiff(myScreenshot, anotherScreenshot);
+BufferedImage diffImage = diff.getMarkedImage(); // comparison result with marked differences
+```
+
+#####Several elements comparison
+`(since 1.2)`  
+Sometimes one need to take a screenshot of several independent elements. In this case aShot computes complex comparison area.
+```java
+List<WebElement> elements = webDriver.findElements(By.cssSelector("#my_element, #popup"));
+new AShot()
+  .withCropper(new IndentCropper() 
+                 .addIndentFilter(blur()))
+  .takeScreenshot(webDriver, elements);
+```
+Here is the result.
+![complex comparison area](/doc/img/complex_elements.png)
+
+One can see only specified elements (the header and the popup) are focused and will be compared if needed.
+
+#####Ignoring of pixels with predefined color
+You can set the color of pixels which should be excluded from comparison of screenshots.
+```java
+ImageDiffer imageDifferWithIgnored = new ImageDiffer().withIgnoredColor(Color.MAGENTA);
+ImageDiff diff = imageDifferWithIgnored.makeDiff(templateWithSomeMagentaPixels, actualScreenshot);
+assertFalse(diff.hasDiff());
+```
+Any pixels in template with color MAGENTA (255, 0, 255 in RGB) will be ignored during comparison.
